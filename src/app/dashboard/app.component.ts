e0de92b3bf13e90c5189428736e625dfa5e0adf1
ghjimchris <jimrajchris@gmail.com>
2020-07-31 07:55:41 +0000

    nav bar designed

---
+++ src/app/dashboard/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {NavComponent } from '../dashboard/nav/nav.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '../app.component.html',
+  styleUrls: ['../app.component.css', '../../assets/bootstrap.css']
+})
+export class AppComponent {
+  title = 'my-app';
+}
